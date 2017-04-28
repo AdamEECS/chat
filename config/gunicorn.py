1e84396f3ecb72eae9dc065551519566bc87015c
suzumiya <jrgwyz@163.com>
2017-04-28 20:39:31 +0800

    增加config目录，项目使用supervisor监控，nginx反向代理

---
+++ config/gunicorn.py
@@ -0,0 +1,2 @@
+bind = '0.0.0.0:8002'
+pid = '/tmp/chat.pid'
---
+++ init.sh
@@ -0,0 +1,3 @@
+ln -s /home/file/chat/config/supervisor.conf /etc/supervisor/conf.d/chat.conf
+
+ln -s /home/file/chat/config/nginx.conf /etc/nginx/sites-enabled/chat
