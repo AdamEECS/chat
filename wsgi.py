4f8b91c8342b871e3f1afdad11a743d35e79a28a
suzumiya <jrgwyz@163.com>
2017-04-28 20:51:08 +0800

    wsgi路径确定

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,15 @@
-import redischat as main
+#!/usr/bin/env python3
 
-app = main.configured_app()
+import sys
+from os.path import abspath
+from os.path import dirname
+
+sys.path.insert(0, abspath(dirname(__file__)))
+
+
+import redischat as main
+application = main.configured_app()
+
 
 # gunicorn -b '0.0.0.0:3000' wsgi:app
 # nohup gunicorn -b '0.0.0.0:3000' wsgi:app &
