912fab067d4c6cd22e8521d0f86fbd1816333d60
suzumiya <jrgwyz@163.com>
2017-04-28 23:01:33 +0800

    worker_class改为eventlet

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -1,2 +1,3 @@
 bind = '0.0.0.0:8002'
 pid = '/tmp/chat.pid'
+worker_class = 'eventlet'
--- wsgi.py
+++ wsgi.py
@@ -11,5 +11,5 @@ import redischat as main
 application = main.configured_app()
 
 
-# gunicorn -b '0.0.0.0:3000' wsgi:app
+# gunicorn -b '0.0.0.0:8002' wsgi --worker-class eventlet -w 1
 # nohup gunicorn -b '0.0.0.0:3000' wsgi:app &
