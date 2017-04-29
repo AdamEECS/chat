ad7092d5c236b55e508bafcf0c7582d2d1bfbf04
suzumiya <jrgwyz@163.com>
2017-04-29 23:32:52 +0800

    启动须知

--- wsgi.py
+++ wsgi.py
@@ -13,3 +13,15 @@ application = main.configured_app()
 
 # gunicorn -b '0.0.0.0:8002' wsgi --worker-class eventlet -w 1
 # nohup gunicorn -b '0.0.0.0:3000' wsgi:app &
+'''
+注意：
+
+1，使用gunicorn启动带stream的项目时，
+worker_class应改为asyn（默认为sync）
+建议改为eventlet或gevent
+若从py文件读配置，参照本项目的格式
+
+2，使用nginx做stream项目的服务器时，
+proxy_buffering需要设置为off
+
+'''
